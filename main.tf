64d20befec2e6a27ac367da363bf77ce9ce64c7b
abc <abc@gmail.com>
2024-04-21 19:03:40 +0530

    terraform Ec2 crea

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0a55ba1c20b74fc30"
+  ami           = "ami-0715c1897453cabd1"
   instance_type = "t2.micro"
-
   tags = {
     Name = "linux"
   }
