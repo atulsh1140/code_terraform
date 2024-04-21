a926d7988da6ed06fec213c399c792187338bf1d
abc <abc@gmail.com>
2024-04-21 18:39:07 +0530

    ami

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-032346ab877c418af"
   instance_type = "t3.micro"
 
   tags = {
     Name = "linux"
   }
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
 }
